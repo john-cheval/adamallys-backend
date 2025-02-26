b27417a02bcd6a463c7bd1a7828da2e45da74d1e
john-cheval <johncheval1@gmail.com>
2025-02-26 12:03:50 +0400

    host name changed

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  // host: env('HOST', '0.0.0.0'),
-  host: "localhost",
+  host: env("HOST", "0.0.0.0"),
+  // host: "localhost",
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
